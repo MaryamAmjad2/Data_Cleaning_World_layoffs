b009b04f5925d595dfa403015df8cbd76dbf4163
Maryam Amjad <87093456+MaryamAmjad2@users.noreply.github.com>
2025-01-24 17:36:44 +0500

    Update explore.sql

--- layoffs dataset/explore.sql
+++ layoffs dataset/explore.sql
@@ -46,8 +46,29 @@ where substr(`date`,1,7) is not null
 group by `month`
 order by 1 asc
 )
-select `month`, SUM(total) over(order by `month`) as Rolling_Total
+select `month`, total ,SUM(total) over(order by `month`) as Rolling_Total
 from Rolling_total;
+
+
+with company_year as (
+select company,
+year(`date`)as years, 
+SUM(total_laid_off) as total
+from layoffs_stagging2
+group by company,year(`date`)
+order by 3 desc
+),
+
+company_year_rank as(
+select * ,dense_rank()
+over(partition by years order by total desc ) as ranking
+from company_year
+where total is not null
+)
+
+select * from company_year_rank
+where ranking<=5
+;
 
 
 
